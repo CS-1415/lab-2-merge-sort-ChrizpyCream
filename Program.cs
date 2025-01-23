e8bf04e12bf46f8fe0e0ecd3e15bb350cd5e6686
Christopher Hercules <chriherc215@gmail.com>
2025-01-22 20:36:32 -0700

    finnished with the combinearrays

--- Program.cs
+++ Program.cs
@@ -9,12 +9,36 @@ public class TDD
     {
        System.Diagnostics.Debug.Assert(Enumerable.SequenceEqual(CombineArrays(new int[]{1, 3, 5}, new int []{-5, 3, 6, 7}), new int[]{-5, 1, 3, 3, 5, 6, 7}));
     System.Diagnostics.Debug.Assert(Enumerable.SequenceEqual(CombineArrays(new int[]{-5, 2, 5, 8, 10}, new int[]{1, 2, 5}), new int[]{-5, 1, 2, 2, 5, 5, 8, 10}));
-    System.Diagnostics.Debug.Assert(Enumerable.SequenceEqual(SortViaMergesort(new int[]{6, 1, -5, 3, 5, 3, 7}), new int[]{-5, 1, 3, 3, 5, 6, 7}));
-    System.Diagnostics.Debug.Assert(Enumerable.SequenceEqual(SortViaMergesort(new int[]{1, 10, -5, 2, 5, 2, 5, 8}), new int[]{-5, 1, 2, 2, 5, 5, 8, 10}));
-    Console.WriteLine("pass");
+    System.Diagnostics.Debug.Assert(Enumerable.SequenceEqual(Mergesort(new int[]{6, 1, -5, 3, 5, 3, 7}), new int[]{-5, 1, 3, 3, 5, 6, 7}));
+    System.Diagnostics.Debug.Assert(Enumerable.SequenceEqual(Mergesort(new int[]{1, 10, -5, 2, 5, 2, 5, 8}), new int[]{-5, 1, 2, 2, 5, 5, 8, 10}));
+    Console.WriteLine("PASS");
     }
     static int[] CombineArrays(int[] a, int[] b)
-    {
-        
-    }
+        {
+            int[] combined = new int[a.Length + b.Length];
+            int aIndex = 0, bIndex = 0, combinedIndex = 0;
+            while (aIndex < a.Length && bIndex < b.Length)
+            {
+                if(a[aIndex] < b[bIndex])
+                {
+                    combined[combinedIndex++] = a[aIndex++];
+                }
+                else
+                {
+                    combined[combinedIndex++] = b[bIndex++];
+                }
+            }
+            while (aIndex < a.Length)
+            {
+                combined[combinedIndex++] = a[aIndex];
+            }
+            while (bIndex < b.Length)
+            {
+                combined[combinedIndex++] = b[bIndex++];
+            }
+            return combined;
+        }
+
+    public static int[] Mergesort(int[] values)
+    {}
 }
